3d371007d21f71c519da7b8f150af2550d1fa10b
Joe <38415384+joe247@users.noreply.github.com>
2019-09-06 09:16:35 +0530

    Added Question

--- Week 3/Programming Assignment 1 - Max And Min.py
+++ Week 3/Programming Assignment 1 - Max And Min.py
@@ -1,3 +1,24 @@
+'''
+Given a list of numbers (integers), find second maximum and second minimum in this list.
+
+>>Input Format:
+
+The first line contains numbers separated by a space.
+
+>>Output Format:
+
+Print second maximum and second minimum separated by a space
+
+>>Example:
+
+Input:
+
+1 2 3 4 5
+
+Output:
+
+4 2
+'''
 A = list(map(int, input().split()))
 A.sort()
 print(A[-2],A[1],end="")
