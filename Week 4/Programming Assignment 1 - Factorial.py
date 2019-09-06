e762bb2e5ed58d8b168f973c5ed454cd74709eee
Joe <38415384+joe247@users.noreply.github.com>
2019-09-06 09:24:26 +0530

    Added Question

--- Week 4/Programming Assignment 1 - Factorial.py
+++ Week 4/Programming Assignment 1 - Factorial.py
@@ -1,3 +1,22 @@
+'''
+Given an integer number n, you have to print the factorial of this number. To know about factorial please click on this link.
+
+>>Input Format:
+
+A number n.
+
+>>Output Format:
+
+Print the factorial of n.
+
+>>Example:
+
+Input:
+4
+
+Output:
+24 
+'''
 from math import factorial
 x=int(input())
 print(factorial(x), end="")
