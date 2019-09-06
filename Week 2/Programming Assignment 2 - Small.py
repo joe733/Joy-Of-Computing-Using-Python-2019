0260fded750ceadd39621e8b058bf4ff8fe41d97
Joe <38415384+joe247@users.noreply.github.com>
2019-09-06 09:12:48 +0530

    Added Quesion

--- Week 2/Programming Assignment 2 - Small.py
+++ Week 2/Programming Assignment 2 - Small.py
@@ -1,2 +1,19 @@
+'''
+Given two numbers (integers) as input, print the smaller number.
+
+>>Input Format:
+The first line of input contains two numbers separated by a space
+
+>>Output Format:
+Print the smaller number
+
+>>Example:
+
+Input:
+2 3
+
+Output:
+2 
+'''
 x,y = map(int, input().split())
 print((x if x<y else y), end="")
